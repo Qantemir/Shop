c37770aab1275e70deccd6a3b5d331e0fa307877
Qantemir <msHorrid@gmail.com>
2025-07-22 13:57:09 +0500

    modified:   handlers/user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1043,16 +1043,17 @@ async def show_delivery_info(callback: CallbackQuery, state: FSMContext):
     await state.update_data(help_message_id=help_msg.message_id)
     await callback.answer()
 
-router.callback_query(F.data == "help_contact")
+@router.callback_query(F.data == "help_contact")
 async def show_contact_help(callback: CallbackQuery, state: FSMContext):
     try:
         await delete_previous_callback_messages(callback, state, "help")
     except Exception as e:
         user_log.error(f"Произошла ошибка при удалении придыдуших сообшений: {e}")
     
-    text="""🤙Контакты
-    ⬇️Telegram для связи⬇️
-        @tikto7182
+    text="""
+    🤙Возникли проблемы?
+⬇️Telegram для связи⬇️
+            @tikto7182
     """
     await safe_delete_message(callback.message)
     help_msg = await callback.message.answer(text, reply_markup=help_menu())
